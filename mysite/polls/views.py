fce4272aab84168fbadf75cd04986e4dc7dae082
nextwebb <oaikhenahpeterson@gmail.com>
2020-06-21 23:59:43 +0100

    playing with urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Awesome job guys!  This is the index page, of our polls application')
