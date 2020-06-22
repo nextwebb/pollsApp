06c629f72347b11414fcb96528a64a66e245ae6c
nextwebb <oaikhenahpeterson@gmail.com>
2020-06-22 04:15:30 +0100

    T for Templates!

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question, Choice
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Awesome job guys!  This is the index page, of our polls application')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    #List Comprehensions
+    # output =", ".join(q.question_text for q in latest_questions)
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+     print(request)
+     return HttpResponse(f"This is the detail view  of the question: {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"These  are the results of the question: {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Vote on question:  {question_id}")
+
+
+
+
